b2b2451e728593d028552b392daa0c1765f35bdc
DanielPinigin <danielpinigin@hanovernorwichschools.org>
2017-05-16 14:37:02 -0400

    Updated from Brython Server: 5/16/2017 2:37:02 PM

--- FindIt.py
+++ FindIt.py
@@ -3,9 +3,19 @@ from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Fra
 SCREEN_WIDTH = 0
 SCREEN_HEIGHT = 0
 
-
-
-print("I <3 Computer Programming!!!")
+class Spaceship(Sprite):
+    
+    asset = ImageAsset("images/four_spaceship_by_albertov_with_thrust.png", 
+        Frame(227,0,292-227,125), 4, 'vertical')
+    width = 80
+    height = 76
+    
+    def __init__(self, position):
+        super().__init__(Spaceship.asset, position)
+        self.fxcenter = 0.5
+        self.fycenter = 0.5
+        self.circularCollisionModel()
+        
 
 
 class FindGame(App):
@@ -13,10 +23,10 @@ class FindGame(App):
         super().__init__(width, height)
         black = Color(0, 1)
         noline = LineStyle(0, black)
-        asset = ImageAsset("lel.jpg")
-        for x in range(self.width//600 + 1):
-            for y in range(self.height//600 + 1):
-                Sprite(asset,(x*600, y*600))
-
+        asset = ImageAsset("images/starfield.jpg")
+        for x in range(self.width//512 + 1):
+            for y in range(self.height//512 + 1):
+                Sprite(asset,(x*512, y*512))
+            Spaceship((.5*(self.width//512+1)*512,.5*(self.height//512+1)*512))
 myapp = FindGame(SCREEN_WIDTH, SCREEN_HEIGHT)
 myapp.run()
